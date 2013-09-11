3bb2b4ccd812264e1df5e65052fbafd68bd992d0
Artem Maschenko <artem.maschenko@gmail.com>
2013-09-11 18:10:42 +0300

    Convert response to json in test helper

--- lib/rodent/test_helpers.rb
+++ lib/rodent/test_helpers.rb
@@ -1,8 +1,10 @@
+require 'multi_json'
+
 module Rodent
   module Test
     module Helpers
       def request(path, *args)
-        @rodent_response = api.route(path).call(*args)
+        @rodent_response = MultiJson.load(api.route(path).call(*args))
       end
 
       def response
